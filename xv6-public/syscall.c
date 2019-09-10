26c3479c4f11d8fa4ab5c973c2b1e5b219bf754f
VictorGuido <a01630603@itesm.mx>
2019-09-10 13:22:37 +0000

    RebooteImpresion

--- xv6-public/syscall.c
+++ xv6-public/syscall.c
@@ -104,6 +104,7 @@ extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_shutdown(void);
+extern int sys_reboot(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -128,6 +129,7 @@ static int (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_shutdown] sys_shutdown,
+[SYS_reboot] sys_reboot,
 };
 
 void
@@ -136,9 +138,37 @@ syscall(void)
   int num;
   struct proc *curproc = myproc();
 
+  char* calls_names[23];
+  //asignar los nombres a cada llamada de sistema
+  calls_names[0] = "sys_fork";
+  calls_names[1] = "sys_exit";
+  calls_names[2] = "sys_wait";
+  calls_names[3] = "sys_pipe";
+  calls_names[4] = "sys_read";
+  calls_names[5] = "sys_kill";
+  calls_names[6] = "sys_exec";
+  calls_names[7] = "sys_fstat";
+  calls_names[8] = "sys_chdir";
+  calls_names[9] = "sys_dup";
+  calls_names[10] = "sys_getpid";
+  calls_names[11] = "sys_sbrk";
+  calls_names[12] = "sys_sleep";
+  calls_names[13] = "sys_uptime";
+  calls_names[14] = "sys_open";
+  calls_names[15] = "sys_write";
+  calls_names[16] = "sys_mknod";
+  calls_names[17] = "sys_unlink";
+  calls_names[18] = "sys_link";
+  calls_names[19] = "sys_mkdir";
+  calls_names[20] = "sys_close";
+  calls_names[21] = "sys_shutdown";
+  calls_names[22] = "sys_reboot";
+
+
   num = curproc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     curproc->tf->eax = syscalls[num]();
+    cprintf("%s -> %d", calls_names[num], num);
   } else {
     cprintf("%d %s: unknown sys call %d\n",
             curproc->pid, curproc->name, num);
