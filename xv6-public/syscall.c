a359eabdf536d6bc7da3e31388fa88e9d074cef6
VictorGuido <a01630603@itesm.mx>
2019-09-10 18:34:22 +0000

    Printcorrecto

--- xv6-public/syscall.c
+++ xv6-public/syscall.c
@@ -168,7 +168,7 @@ syscall(void)
   num = curproc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     curproc->tf->eax = syscalls[num]();
-    cprintf("%s -> %d", calls_names[num], num);
+    cprintf("%s -> %d", calls_names[num-1], num);
   } else {
     cprintf("%d %s: unknown sys call %d\n",
             curproc->pid, curproc->name, num);
